8b066f83a667fe085d67216c0b9a1452a55cd396
NGUYENTAMLONG <rongden_pikachu@yahoo.com.vn>
2021-12-08 10:14:53 +0700

    fix 3

--- controller/getAllJobs.js
+++ controller/getAllJobs.js
@@ -5,6 +5,7 @@ const getAllJobs = async (req, res) => {
     const allJobs = await Jobs.find();
     if (!allJobs) throw Error("Not Found !!! -_-");
     res.status(200).json(allJobs);
+    res.send(allJobs);
   } catch (error) {
     res.status(400).json({ msg: `${error}` });
   }
